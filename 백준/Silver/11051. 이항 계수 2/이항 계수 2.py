a4691b116efa7532142ddda05de80ed6e70fe246
jaesung4231 <102843446+jaesung4231@users.noreply.github.com>
2022-08-21 16:11:04 +0900

    [Silver II] Title: 이항 계수 2, Time: 92 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,14 @@
+import sys
+n,k=map(int,sys.stdin.readline().split())
+result=1
+
+for i in range(k):
+    result *=n
+    n-=1
+
+divisor = 1
+
+for i in range(2, k+1):
+    divisor *=i
+
+print((result//divisor)%10007)
