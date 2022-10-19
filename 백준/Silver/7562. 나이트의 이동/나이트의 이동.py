64d70371565ee92de896a87716956df67a4cfacc
jaesung4231 <102843446+jaesung4231@users.noreply.github.com>
2022-10-19 19:33:18 +0900

    [Silver I] Title: 나이트의 이동, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,36 +1,62 @@
-from collections import deque
-from copy import deepcopy
 import sys
-input=sys.stdin.readline
-N=int(input())
-cb=[]
+input = sys.stdin.readline
 
 
-dx=[-2,-2,-1,-1,2,2,1,1]
-dy=[1,-1,2,-2,-1,1,-2,2]
+dx = [-2, -2, -1, -1, 1, 1, 2, 2]
+dy = [-1, 1, -2, 2, -2, 2, -1, 1]
 
-def bfs(x,y,board):
-    queue=deque()
-    queue.append([x,y])
-    while(queue):
-        a,b=queue.popleft()
-        for i in range(8):
-            nx=a+dx[i]
-            ny=b+dy[i]
-            if -1<nx<T and -1<ny<T and board[nx][ny]==0:
-                board[nx][ny]=board[a][b]+1
-                queue.append([nx,ny])
+def bfs_bound(bd, start, end):
+    if start == end:
+        return 0
+    que = [start]
+    visited = {start}
+    m = 1
+    while True:
+        new_que = []
+        for x, y in que:
+            for i, j in zip(dx, dy):
+                nx, ny = x+i, y+j
+                if 0 <= nx < bd and 0 <= ny < bd:
+                    new = nx, ny
+                    if new not in visited:
+                        visited.add(new)
+                        new_que.append(new)
+        if end in visited:
+            return m
+        m += 1
+        que = new_que
 
-        
-for i in range(N):
-    T=int(input())
-    cb=[]
-    for j in range(T):
-        cb.append([0]*T)
-    x,y=map(int, input().split())
-    cb[x][y]=1
-    bfs(x,y,cb)
-    dtx,dty=map(int,input().split())
-    print(cb[dtx][dty]-1)
+def bfs(p):
+    if p == (0, 0):
+        return 0
+    start = (0, 0)
+    que = [start]
+    visited = {start}
+    for m in range(1, 10):
+        new_que = []
+        for x, y in que:
+            for i, j in zip(dx, dy):
+                new = x + i, y + j
+                if new not in visited:
+                    visited.add(new)
+                    new_que.append(new)
+        if p in visited:
+            return m
+        que = new_que
 
+def manual(x, y):
+    i = 0
+    while x >= 5 or y >= 5:
+        x, y = (x-2, abs(y-1)) if x > y else (abs(x-1), y-2)
+        i += 1
+    return i + bfs((x, y))
 
+for _ in range(int(input())):
+    bd = int(input())
+    x1, y1 = map(int, input().split())
+    x2, y2 = map(int, input().split())
+    x, y = abs(x1-x2), abs(y1-y2)
+    if x >= 5 or y >= 5:
+        print(manual(x, y))
+    else:
+        print(bfs_bound(bd, (x1, y1), (x2, y2)))
