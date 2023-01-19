733153536c0f086fab126fb671f5812330ff38dd
jaesung4231 <102843446+jaesung4231@users.noreply.github.com>
2023-01-19 16:58:16 +0900

    [Silver III] Title: N과 M (1), Time: 256 ms, Memory: 123680 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,9 +1,21 @@
-from itertools import permutations
 import sys
-N,M=map(int,sys.stdin.readline().split())
-data=[]
-for i in range(1,N+1):
-    data.append(i)
-result=(permutations(data,M))
-for i in result:
-    print(*i)
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+visited=[0 for _ in range(n+1)]
+def dfs(L,n,m,p):
+    if L==m :
+        for i in p:
+            print(i, end=" ")
+        print()
+    else:
+        for i in range(1,n+1):
+            if visited[i]==1:
+                continue
+            p.append(i)
+            visited[i]=1
+            dfs(L+1,n,m,p)
+            p.pop()
+            visited[i]=0
+                       
+dfs(0,n,m,[])
