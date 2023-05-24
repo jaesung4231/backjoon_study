b8af218582e75749b78260dd39ce81b94b870e66
jaesung4231 <102843446+jaesung4231@users.noreply.github.com>
2023-05-24 16:39:41 +0900

    [Gold V] Title: 수 고르기, Time: 200 ms, Memory: 37788 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -1,34 +1,24 @@
-from collections import deque, defaultdict
+from itertools import combinations
+from collections import deque
+import math
 import sys
 input=sys.stdin.readline
-
 n,m=map(int,input().split())
 number=[]
-for i in range(n):
+for i in  range(n):
     number.append(int(input()))
 number.sort()
-# print(number)
 left=0
 right=0
-answer=2000000000
-
-while left!=n-1:
-    S=number[right]-number[left]
-    if S==m:
-        answer=S
-        break
-    if S<answer and m<=S:
-        answer=S
-    if S<m and right<n-1:
-        right+=1
-    else:
+ans=2000000000
+while left<n:
+    cost=number[right]-number[left]
+    if cost>=m:
+        # print(left,right,cost)
+        ans=min(ans,cost)
         left+=1
-
-print(answer)
-
-
-
-    
-
-
-
+    else:
+        right+=1
+        if right>(n-1):
+            break
+print(ans)
