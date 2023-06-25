d29e8f8544923bec7470003b78bbbe9d285f3a73
jaesung4231 <102843446+jaesung4231@users.noreply.github.com>
2023-06-25 23:15:20 +0900

    [Gold V] Title: 블랙 프라이데이, Time: 172 ms, Memory: 117392 KB -BaekjoonHub

--- 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.py
+++ 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.py
@@ -1,42 +1,52 @@
+from collections import deque, defaultdict
+from itertools import combinations, product, permutations
+from copy import deepcopy
+import heapq
 import sys
+input=sys.stdin.readline
+n,c=map(int,input().split())
+weight=list(map(int,input().split()))
+weight.sort()
+two=defaultdict(int)
+ans=0
+for i in range(n):
+    two[weight[i]]
 
-def binarySearch(start, end, c, weights):
-    while start <= end:
-        mid = (start + end) // 2
-        if weights[mid] == c:
-            return mid
-        if weights[mid] < c:
-            start = mid + 1
-        else:
-            end = mid - 1
-    return -1
 
-def event(n, c, weights):
-    if binarySearch(0, n - 1, c, weights) >= 0:# 고른 물건 1개
-        return True
-    i = 0
-    j = n - 1
-    while(i < j): # 고른 물건 2개 or 3개
-        weightSum = weights[i] + weights[j]
-        if weightSum > c:
-            j -= 1
-        elif weightSum == c: # 고른 물건 2개
-            return True
-        else:
-            diff = c- weightSum
-            if diff != weights[i] and diff != weights[j] and binarySearch(0, n - 1, diff, weights) >= 0:
-                return True # 고른 물건 3개
-            i += 1
-    return False
-
-# N : 물건 개수, C : 무게
-N, C = map(int, sys.stdin.readline().split())
-
-# N개의 물건의 무게들
-weights = list(map(int, sys.stdin.readline().split()))
-weights.sort()
-
-if event(N, C, weights):
-    print(1)
+if c in two:
+    ans=1
 else:
-    print(0)
+    left=0
+    right=n-1
+    while left<=right:
+        mid=(left+right)//2
+        w=weight[mid]
+        if w==c:
+            ans=1
+            break
+        if w>c:
+            right=mid-1
+        else:
+            left=mid+1
+    
+    
+    if ans==0:
+        left=0
+        right=n-1
+        while left<right:
+            total=weight[left]+weight[right]
+            if total==c:
+                ans=1
+                break
+            if total>c:
+                right-=1
+            else:
+                diff=c-total
+                if diff!=weight[left] and diff!=weight[right] and (diff in two):
+                    ans=1
+                    break
+                left+=1
+
+print(ans)
+ 
+        
