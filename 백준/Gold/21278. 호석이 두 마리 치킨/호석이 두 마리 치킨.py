4b46cd528a0b8b5a8eb695e64f6b1968ca4af819
jaesung4231 <102843446+jaesung4231@users.noreply.github.com>
2023-07-07 17:34:32 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 3464 ms, Memory: 34184 KB -BaekjoonHub

---
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+from itertools import combinations
+input=sys.stdin.readline
+n,m=map(int,input().split())
+graph=[[] for _ in range(n)]
+dis_ans=1e9
+
+for i in range(m):
+    a,b=map(int,input().split())
+    graph[a-1].append(b-1)
+    graph[b-1].append(a-1)
+
+def bfs(x,y,visited):
+    queue=deque()
+    queue.append(x)
+    total=0
+    while queue:
+        a=queue.popleft()
+        for g in graph[a]:
+            if g==x or g==y:
+                continue
+            if visited[g]==0 or visited[g]>visited[a]+1:
+                queue.append(g)
+                visited[g]=visited[a]+1
+                total+=visited[g]
+    # print(total,visited)
+    return total
+
+
+for com in combinations((range(0,n)),2):
+    a,b=com[0],com[1]
+    visited=[0]*n
+    dis1=bfs(a,b,visited)
+    dis2=bfs(b,a,visited)
+    if dis1+dis2<dis_ans:
+        dis_ans=dis1+dis2
+        answer=sorted(com)
+    
+print(answer[0]+1,answer[1]+1,dis_ans*2)
+
