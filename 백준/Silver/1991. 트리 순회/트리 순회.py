6c283aa0fc36a87f93cc7fbfc6f512fcb3527bda
jaesung4231 <102843446+jaesung4231@users.noreply.github.com>
2023-07-11 21:23:23 +0900

    [Silver I] Title: 트리 순회, Time: 64 ms, Memory: 34168 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,35 @@
+from itertools import permutations
+from collections import deque, defaultdict
+import sys
+input=sys.stdin.readline
+n=int(input())
+tree=defaultdict(list)
+for i in range(n):
+    a,b,c=input().split()
+    tree[a]=[b,c]
+
+
+def preorder (root):
+    if root!='.':
+        print(root, end="")
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+def inorder(root):
+    if root!='.':
+        inorder(tree[root][0])
+        print(root, end="")
+        inorder(tree[root][1])
+
+def postorder(root):
+    if root!='.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end="")
+
+
+preorder("A")
+print()
+inorder("A")
+print()
+postorder("A")
