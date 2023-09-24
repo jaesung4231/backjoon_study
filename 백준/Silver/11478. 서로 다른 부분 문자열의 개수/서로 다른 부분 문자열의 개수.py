e48874cdec1ee8fa89344542eafc87bac227d9b4
jaesung4231 <102843446+jaesung4231@users.noreply.github.com>
2023-09-24 20:45:59 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 548 ms, Memory: 318276 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -1,13 +1,17 @@
 import sys
-S=sys.stdin.readline().strip()
-data=[]
-for i in range(len(S)):
-  start=0
-  end=i+1
-  while(end<=len(S)):
-    data.append(S[start:end])
-    end+=1
-    start+=1
+from itertools import permutations
+input=sys.stdin.readline
+word=set()
+S=input().strip()
+word.add(S)
 
-result=set(data)
-print(len(result))
+for s in S:
+    word.add(s)
+
+for i in range(1,len(S)):
+    s=0
+    while s+i<len(S)+1:
+        word.add(S[s:s+i])
+        s+=1
+
+print(len(word))
