bbdf8861e27c34d4e84d9a84931f89727d5a6a70
jaesung4231 <102843446+jaesung4231@users.noreply.github.com>
2023-10-10 17:31:19 +0900

    [Silver III] Title: 문자열 집합, Time: 348 ms, Memory: 120576 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,12 +1,14 @@
 import sys
-M_num,S_num=map(int,sys.stdin.readline().split())
-M=[]
-S=[]
-count=0
-for i in range(M_num):
-  M.append(input())
-for j in range(S_num):
-    S=input()
-    if S in M:
-        count+=1
-print(count)
+import math
+from collections import deque, defaultdict
+from itertools import combinations
+N,M=map(int,input().split())
+table=defaultdict(int)
+for _ in range(N):
+    table[input().strip()]
+ans=0
+for _ in range(M):
+    word=input().strip()
+    if word in table:
+        ans+=1
+print(ans)
