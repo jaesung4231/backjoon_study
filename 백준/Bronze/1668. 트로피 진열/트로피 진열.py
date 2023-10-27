8ba1ffbb4cb4a5216537b672be08b2012d0724c7
jaesung4231 <102843446+jaesung4231@users.noreply.github.com>
2023-10-27 21:17:59 +0900

    [Bronze II] Title: 트로피 진열, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/1668. 트로피 진열/트로피 진열.py
+++ 백준/Bronze/1668. 트로피 진열/트로피 진열.py
@@ -10,16 +10,18 @@ for _ in range(N):
 
 canSee=[1]*N
 for i in range(N):
-    for j in range(i+1,N):
-        if trophy[j]<=trophy[i]:
-            canSee[j]=0
+    if canSee[i]:
+        for j in range(i+1,N):
+            if trophy[j]<=trophy[i]:
+                canSee[j]=0
 
 print(canSee.count(1))          
 trophy.reverse()
 
 canSee=[1]*N
 for i in range(N):
-    for j in range(i+1,N):
-        if trophy[j]<=trophy[i]:
-            canSee[j]=0
+    if canSee[i]:
+        for j in range(i+1,N):
+            if trophy[j]<=trophy[i]:
+                canSee[j]=0
 print(canSee.count(1))
